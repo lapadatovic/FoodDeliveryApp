34352928b3f371a1350e179ad3d70f50f7edd605
Danijel Lapadatovic <l.danijel977@gmail.com>
2023-05-09 22:51:24 +0200

    Changed img folder destinatination and set Routes

--- food-delivery-app/src/App.js
+++ food-delivery-app/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom'
-import { Header } from './components';
+import { Header, MainContainer, CreateContainer } from './components';
 
 function App() {
   return (
     <div className="w-screen h-auto flex flex-col bg-primary">
       <Header />
 
-      <main className='mt-24 p-8 w-full'>Main Container</main>
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path='/' element={<MainContainer />} />
+          <Route path='/createItem' element={<CreateContainer />} />
+        </Routes>
+      </main>
     </div>
   );
 }
