52dd262e19e0c79d971c9a855b467b0aabcc3309
Danijel Lapadatovic <l.danijel977@gmail.com>
2023-05-18 17:26:46 +0200

    Created mobile version of header menu

--- food-delivery-app/src/components/Header.jsx
+++ food-delivery-app/src/components/Header.jsx
@@ -33,15 +33,19 @@ export default function Header() {
       setIsMenu(!isMenu)
     }
   }
+
+  const logout = () => {
+    console.log('logged out')
+  }
   
   return (
-    <header className='fixed z-50 w-screen p-6 px-16'>
+    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
         {/*  Desktop and Tablet */}
         <div className='hidden md:flex w-full h-full items-center justify-between'> 
         {/* Logo */}
           <div className='flex items-center gap-2'>
             <Link to={"/"}>
-              <img className='w-8 object-cover' src={Logo} alt="LogoImg"/>
+              <img  className='w-8 object-cover'src={Logo}  alt="LogoImg"/>
               <p className='text-headingColor text-xl font-bold'>Chicky</p>
             </Link>
           </div>
@@ -95,6 +99,7 @@ export default function Header() {
                       )
                     }
                     <p 
+                      onClick={logout}
                       className='flex px-4 py-2 items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>
                         Logout <MdLogout/> 
                     </p>
@@ -105,9 +110,59 @@ export default function Header() {
             
           </div>
         </div>
-        {/* Mobile */}
-        <div className='flex md:hidden w-full h-full'>
 
+        {/* Mobile */}
+        <div className='flex items-center justify-between md:hidden w-full h-full'>
+          <Link to={"/"}>
+            <img  className='w-8 object-cover'src={Logo}  alt="LogoImg"/>
+            <p className='text-headingColor text-xl font-bold'>Chicky</p>
+          </Link>
+          <div className='relative'>
+              <motion.img whileTap={{scale: 0.6}}
+                className=' object-cover rounded-full w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl' 
+                src={user ? user.photoURL : Avatar} 
+                alt=""
+                onClick={login}
+                referrerPolicy="no-referrer"
+              />
+
+              {
+                isMenu && ( 
+                  <motion.div 
+                    initial ={{ opacity: 0, scale: 0.6 }}
+                    animate ={{ opacity: 1, scale: 1   }}
+                    exit    ={{ opacity: 0, scale: 0.6 }}
+                    className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 py-2'
+                  >
+                    {
+                      user && user.email === 'lapadatovic.dev@gmail.com' && (
+                        <Link to='/createItem'>
+                          <p 
+                            className='flex px-4 py-2 items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' 
+                          >
+                            New item <MdAdd/>
+                          </p>
+                        </Link>
+                        
+                      )
+                    }
+
+                  <ul
+                    className='flex flex-col '>
+                    <li className='px-4 py-2 hover:bg-slate-100 text-textColor text-base text-color hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+                    <li className='px-4 py-2 hover:bg-slate-100 text-textColor text-base text-color hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
+                    <li className='px-4 py-2 hover:bg-slate-100 text-textColor text-base text-color hover:text-headingColor duration-100 transition-allease-in-out cursor-pointer '>Service</li>
+                    <li className='px-4 py-2 hover:bg-slate-100 text-textColor text-base text-color hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
+                  </ul>
+                    <p 
+                      onClick={logout}
+                      className='flex m-2 p-2 rounded-md  shadow-md justify-center bg-gray-200 items-center gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base'>
+                        Logout <MdLogout/> 
+                    </p>
+                  </motion.div>
+                )
+              }
+            </div>
         </div>
     </header>
   )
